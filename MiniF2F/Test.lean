4b257c3510efaa8c95dbec96b471c9345ea3231b
AG161 <81839540+AG161@users.noreply.github.com>
2024-08-12 18:41:08 -0400

    Fix mathd_algebra_342 in Test.Lean

--- MiniF2F/Test.lean
+++ MiniF2F/Test.lean
@@ -873,8 +873,8 @@ theorem mathd_algebra_196 (S : Finset ℝ) (h₀ : ∀ x : ℝ, x ∈ S ↔ abs 
     (∑ k in S, k) = 4 := by sorry
 #align mathd_algebra_196 mathd_algebra_196
 
-theorem mathd_algebra_342 (a d : ℝ) (h₀ : (∑ k in Finset.range 5, a + k * d) = 70)
-    (h₁ : (∑ k in Finset.range 10, a + k * d) = 210) : a = 42 / 5 :=
+theorem mathd_algebra_342 (a d : ℝ) (h₀ : (∑ k in Finset.range 5, (a + k * d)) = 70)
+    (h₁ : (∑ k in Finset.range 10, (a + k * d)) = 210) : a = 42 / 5 :=
   by sorry
 #align mathd_algebra_342 mathd_algebra_342
 
